80af7570db679342fb9bcb877fb2759c7bc10c55
altynbek695 <shakirov.altyn695@gmail.com>
2022-11-10 17:14:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import tegerek.Circle;
+
+import java.awt.geom.Area;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle.area(123);
+        Circle.circumference(1321);
+    }
+}
---
+++ src/tegerek/Circle.java
@@ -0,0 +1,16 @@
+package tegerek;
+
+public class Circle {
+
+
+
+
+    public static void area(int radius){
+        System.out.println(Math.PI * (radius * radius));
+
+    }
+    public  static void circumference(int radius){
+        System.out.println(Math.PI * 2 * radius);
+
+    }
+}
